d3a1bea230ac67cc0816552ec4bedeee2ff35226
acamargo <acamargo@rocketmail.com>
2016-11-29 17:14:10 +0000

    Added new functions

--- curve_analytics.R
+++ curve_analytics.R
@@ -1,7 +1,9 @@
 library('minpack.lm');
 library("nlmrt")
 library("optimx")
-library(ggplot2)
+library(ggplot2);
+
+source('../senescence_disease/generic.R');
 
 # [64-bit] C:\Program Files\R\R-3.3.1
 
@@ -15,7 +17,7 @@ rhs2 <- function(b, mydata) {
   sum((mydata$y - (((mydata$x*b[2]) / (mydata$x+b[1])) - b[3]))^2)
 }
 
-rhsfit <- function(b, mydata) {
+rhsfit1 <- function(b, mydata) {
   #ydat ~ P25*exp((ha/(8.314*298.15)) - ha/(8.314*tdat))
   sum((mydata$y - (b[1]*exp((b[2]/(8.314*298.15)) - b[2] /(8.314*mydata$x)) ) )^2)
 }
@@ -24,7 +26,7 @@ rhsfit2 <- function(b, mydata) {
   #(P25*exp( (log(1 + exp((S*298.15-hd)/(8.314*298.15))) + (ha/(8.314*298.15))) 
    #         - (ha/(8.314*tdat)))) / (1 + exp((S*tdat-hd)/(8.312*tdat)));
   sum((mydata$y - (  (b[1]*exp(( log(1+ exp((b[3]*298.15-b[4])/(8.314*298.15))) + (b[2]/(8.314*298.15)) 
-                               - (b[2]/(8.314*tdat)))) / (1 + exp((b[3]*tdat-b[4])/(8.314*tdat)))  )) )^2)
+                               - (b[2]/(8.314*mydata$x)))) / (1 + exp((b[3]*mydata$x-b[4])/(8.314*mydata$x)))  )) )^2)
 }
 
 optima = function(start1, w, fc)
@@ -47,7 +49,7 @@ plotCurve = function(x,y, fm, eq, yl, spc, tempr, repl)
 
 searchOutlier = function(data)
 {
-  data = weeddata;
+  library('outliers');
   data = data[order(x),];
   row.names(data) = 1:nrow(data);
   u = chisq.out.test(data$y, variance=var(data$y), opposite = FALSE);
@@ -83,56 +85,70 @@ searchOutlier = function(data)
   
   data = data[-s,];
   row.names(data) = 1:nrow(data);
+  return(data);
 }
 
 
-eqFitcl1 = function(tdat)
+eqFitcl1 = function(tdat, ydat, weeddata)
 {
-  start= c(P25 = 0, ha=38000);
-  eunsc = ydat ~ P25*exp((ha/(8.314*298.15)) - ha/(8.314*tdat));
-  
-}
-
-eqFitcl2 = function(tdat)
-{
-  start= c(P25 = 10, ha=20000, S=800, hd=258000);
-  eunsc = ydat ~ (P25*exp( (log(1 + exp((S*298.15-hd)/(8.314*298.15))) + (ha/(8.314*298.15))) 
-                             - (ha/(8.314*tdat)))) / (1 + exp((S*tdat-hd)/(8.312*tdat)));
-  
-}
-
-# Equation 1
-eqFit = function(x,y)
-{
-  #b1=lo, b2=pgmax, b3=RD
-  r = list()
-  #mx = max(y); mn=min(y); mm=0;
-  tdat = x;
-  ydat = y;
-  
-  
-  
+  o = list();
   weeddata = data.frame(y=ydat, x=tdat);
-  searchOutlier(weeddata);
-  tdat = weeddata$x ;
-  ydat = weeddata$y;
-  optx1 = optima(start1, weeddata, rhsfit);
-  optx1
-  
-  optx = optima(start2, weeddata, rhsfit2);
-  optx
-  
+  o$start= c(P25 = 0, ha=38000);
+  o$eunsc = ydat ~ P25*exp((ha/(8.314*298.15)) - ha/(8.314*tdat));
+  o$optx1 = optima(o$start, weeddata, rhsfit1);
+  o1= nlsLM(o$eunsc, 
+              start=list(P25 = o$optx1$P25[1], ha = o$optx1$ha[1]), trace = TRUE);
+  return(o1);
+}
 
-  o1= nlsLM(eunsc10, 
-            start=list(P25 = optx1$P25[1], ha = optx1$ha[1]), trace = TRUE);
-  o1
+eqFitcl2 = function(tdat, ydat, weeddata)
+{
+  o = list();
+  o$start= c(P25 = 10, ha=20000, S=800, hd=258000);
+  o$eunsc = ydat ~ (P25*exp( (log(1 + exp((S*298.15-hd)/(8.314*298.15))) + (ha/(8.314*298.15))) 
+                             - (ha/(8.314*tdat)))) / (1 + exp((S*tdat-hd)/(8.312*tdat)));
+  o$optx1 = optima(o$start, weeddata, rhsfit2);
+  o2 = nlsLM(o$eunsc, 
+               start=list(P25 = o$optx$P25[1], ha = o$optx$ha[1], S = o$optx$S[1], hd = o$optx$hd[1]), 
+               trace = TRUE);
+  return(o2);
+}
+
+
+getlogistic = function(x, y)
+{
+  library('nlstools');
+  library('nlme');
+  weeddata = data.frame(y=y, x=x);
+  f = as.formula(paste('y', '~', 'SSlogis(x, phi1, phi2, phi3)', sep=''));
+  gr = nls(f, data = weeddata);
+  r = nlsResiduals(gr);
+  i = union(which(r$resi2[1:nrow(weeddata),2] > 1), which(r$resi2[1:nrow(weeddata),2] < -1));
+  # plot(weeddata[['x']], weeddata[['y']], xlab = "DAS", ylab = 'ta');
+  # lines(weeddata[['x']], as.vector(predict(gr)), col="red",lty=2,lwd=3);
+  # text(170, min(log(sub[[trait]]))+1.5, paste(ename, '\n', 'phi1: ', round(alpha[1],2), 
+  #                                             '\n  phi2:', round(alpha[2],2), '\n',  ' phi3:', round(alpha[3],2), sep=''),cex = 0.8);
+  # 
+  return(i);
+}
+# Equation 1
+eqFit = function(xp,yp)
+{
+  r = list()
+  i = getlogistic(xp, yp);
+  tdat = xp[-i];
+  ydat = yp[-i];
+  weeddata = data.frame(y=ydat, x=tdat);
+  r$o1 = eqFitcl1(tdat, ydat, weeddata);
+  r$o2 = eqFitcl2(tdat, ydat, weeddata);
   
-  o = nlsLM(eunsc12, 
-            start=list(P25 = optx$P25[1], ha = optx$ha[1], S = optx$S[1], hd = optx$hd[1]), trace = TRUE);
-  o
+  
+  k = min(ydat) + (min(ydat)/3);
   plot(tdat, ydat);
-  lines(tdat, fitted(o1), col = 2, lwd = 2);
-  
+  lines(tdat, fitted(r$o1), col = 'red', lwd = 2);
+  lines(tdat, fitted(r$o2), col = 'blue', lwd = 2);
+  legend(290,k, legend = c('eq1', 'eq2'), col=c('red', 'blue'), lty=1);
+  r$ydat = ydat;
   return(r)
 }
 
@@ -161,11 +177,11 @@ makeFunctions = function(ydat, tdat)
   elist[['2']] = c(eq = ydat ~ ((tdat * Pgmax)/(tdat + lo)) - RD, 
                    f=rhs2, qy = "(lo*Pgmax) / ((tdat+lo)^2)");
   elist[['10']] = c(eq = ydat ~ P25*exp((c-ha)/(8.314*tdat)), 
-                   f=rhsfit, qy = "");
+                   f=rhsfit1, qy = "");
   return(elist);
 }
 
-# Calculation
+# Calculate growth parameters
 nlsLMFunction = function(x,y, tname, spc, tempr, repl, fn)
 {
   b = c();
@@ -191,8 +207,7 @@ nlsLMFunction = function(x,y, tname, spc, tempr, repl, fn)
   return(b)
 }
 
-
-
+# calculate temperature
 getPar = function(data, varn)
 {
   data = data.frame(data)
@@ -216,12 +231,11 @@ r$I = as.numeric(as.character(r$I));
 r = r[complete.cases(r),];
 row.names(r) = 1:nrow(r);
 b = c();
-break();
 
 
-pdf('curves.pdf');
 
-par(mfrow=c(2,2));
+#pdf('curves.pdf');
+#par(mfrow=c(2,2));
 for(sp in unique(r$specie)[1])
 {
   sub = r[which(r$specie == sp),];
@@ -233,17 +247,28 @@ for(sp in unique(r$specie)[1])
       sub2 = sub1[which(sub1$t == temp),];
       tname= paste(unique(as.character(sub2$specie)), 'Temp:', unique(sub2$t), 'rep: ', unique(sub2$r), sep=' ');
       b = rbind(b, nlsLMFunction(sub2$I, sub2$Pn, tname, unique(as.character(sub2$specie)), 
-                        unique(sub2$t), unique(sub2$r), c('1') ));
+                                 unique(sub2$t), unique(sub2$r), c('1') ));
     }
   }
-}   
+}
+break();
+#dev.off()
 
-b = convert2numeric(data.frame(b), c("eq","rep","lo","Pgmax","RD","temperature"))
-b1 = getPar(b, 'temperature');
-dev.off()
+b = convert2numeric(data.frame(b), c("eq","rep","lo","Pgmax","RD","temperature"));
+b = getPar(b, 'temperature');
+b = data.frame(b);
+b = b[order(b$tk),];
+b$RD = -1*(b$RD);
+# Pgmax
+x = b$tk; y = b$Pgmax;
+m = eqFit(x,y);
+# lo
+x = b$tk; y = b$lo;
+m = eqFit(x,y);
+# RD
+x = b$tk; y = -1*(b$RD);
+m = eqFit(x,y);
 
-
-b= data.frame(b)
 fname = paste('gr_param', '.pdf', sep='');
 pdf(fname);
 for(tname in c('lo', 'Pgmax', 'RD'))
